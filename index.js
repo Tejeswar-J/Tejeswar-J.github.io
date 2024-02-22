a523704b4b8f5a1d7c6a285d6e0898fc8bdacabb
Jyothi venkata Tejeswar reddy <65271874+Tejeswar-J@users.noreply.github.com>
2024-02-22 10:51:47 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <h1>this is the Git Pages</h1>
+    <p>checking if the given data is coming on the web pages </p>
+</body>
+</html>
